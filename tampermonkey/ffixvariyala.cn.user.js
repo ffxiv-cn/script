e2ed5c261575f95d0d4779e5e0a359f3f22a999b
野原小牛 <oyyz@vip.qq.com>
2019-12-27 15:25:14 +0800

    init

---
+++ tampermonkey/ffixvariyala.cn.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         ffixv ariyala中文化脚本
+// @namespace    https://www.neppure.vip/
+// @version      0.1.0
+// @description  ffixv ariyala中文化脚本,翻译数据gists维护
+// @author       野原小牛
+// @match        https://ffxiv.ariyala.com/*
+// @grant        none
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js
+// @require      https://cdn.jsdelivr.net/npm/lodash@4.17.15/lodash.min.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // Your code here...
+})();
+
